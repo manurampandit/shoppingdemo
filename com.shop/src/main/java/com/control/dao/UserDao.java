49aaa4039831f5281a00dfb4030c2cd5bacdc29c
vaibhavcse30 <vaibhavcse30@gmail.com>
2015-03-29 01:54:49 +0530

    Updating with Rest Code Changes

--- com.shop/src/main/java/com/control/dao/UserDao.java
+++ com.shop/src/main/java/com/control/dao/UserDao.java
@@ -1,14 +1,18 @@
 package com.control.dao;
-import java.util.List;  
+
+import java.util.List;
 
 import com.control.model.Users;
 
-  
-public interface UserDao {  
- public void insertData(Users user);  
- public List<Users> getUserList();  
- public void updateData(Users user);  
- public void deleteData(Integer id);  
- public Users getUser(Integer id);  
-  
-}  
+public interface UserDao {
+	public void insertData(Users user);
+
+	public List<Users> getUserList();
+
+	public void updateData(Users user, Integer id);
+
+	public void deleteData(Integer id);
+
+	public Users getUser(Integer id);
+
+}
